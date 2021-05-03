11380bcaff54af98ddf33bb257a4fe9c6a0863ee
Dalton Willard <willarddalton@gmail.com>
2021-05-03 12:33:13 -0600

    Classwork

--- names.py
+++ names.py
@@ -27,3 +27,33 @@ daltons = names.query('name == "Dalton"')
 
 alt.Chart(daltons).encode(x = "year", y = "AZ").mark_circle()
 # %%
+
+bruce = names.query('name == "Bruce"')
+bruce.head()
+
+# %%
+bruce_nation = alt.Chart(bruce).encode(
+    x = alt.X('year', title= "Year", axis = alt.Axis(format = 'd')), y = "Total").mark_line()
+bruce_utah = alt.Chart(bruce).encode(x = "year", y = "UT").mark_line()
+
+alt.layer(bruce_nation, bruce_utah)
+
+# %%
+alt.hconcat(bruce_nation, bruce_utah)
+# %%
+
+max_year = bruce.year[bruce.Total.idxmax()]
+max_amt = bruce.Total.max()
+print(max_year)
+
+# line_df = pd.DataFrame({'year': [max_year]})
+yr_line = alt.Chart(pd.DataFrame({'year': [max_year]})).mark_rule(color = "red").encode(x = "year")
+yr_circle = alt.Chart(pd.DataFrame({'year': [max_year], 'Total': [max_amt]})).mark_circle(color = "red").encode(x = "year", y = "Total")
+
+
+# %%
+
+bruce_nation + yr_line + yr_circle
+# %%
+
+# %%
